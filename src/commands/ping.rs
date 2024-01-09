6909f01789f32ebecdefc4483a45119dd376d002
Lost <pandagaminggr@gmail.com>
2024-01-09 18:03:55 +0200

    feat: implement websocket ping functionality

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -2,9 +2,8 @@ use crate::{Context, Error};
 
 // Ping the bot!
 #[poise::command(slash_command, prefix_command)]
-pub async fn ping(
-    ctx: Context<'_>,
-) -> Result<(), Error> {
-    ctx.say("Pong!").await?;
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    let response = format!{"Pong! ({:?})", ctx.ping().await};
+    ctx.say(response).await?;
     Ok(())
 }
