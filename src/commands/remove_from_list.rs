7452d04fd7836013e56f609769fbf7a23bb49719
Lost <pandagaminggr@gmail.com>
2024-01-17 22:54:24 +0200

    feat: implement remove_from_list command; fix errors

--- src/commands/remove_from_list.rs
+++ src/commands/remove_from_list.rs
@@ -4,8 +4,20 @@ use crate::{Context, Error};
 #[poise::command(prefix_command, slash_command)]
 pub async fn remove_from_list(
     ctx: Context<'_>,
-    #[description = "Specify the project name to add"] entry: Option<String>,
+    #[description = "Specify the project name to remove"] entry: Option<String>,
 ) -> Result<(), Error> {
-    todo!("Implement");
+    let mut projects = ctx.data().projects.lock().await;
+    if entry.is_none() {
+        ctx.say("No entry specified!").await?;
+        return Ok(());
+    }
+    let index = projects.iter().position(|e| e == entry.as_ref().unwrap());
+    if index.is_none() {
+        ctx.say("Could not find entry!").await?;
+        return Ok(());
+    }
+    ctx.say(format! {"Removed `{}` from projects list!", projects[index.unwrap()]})
+        .await?;
+    projects.remove(index.unwrap());
     Ok(())
 }
